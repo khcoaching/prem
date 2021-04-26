c1b428ac9040b31c70523b53862681ddf9fb90bd
khcoaching <khcoaching@github>
2021-04-26 09:42:05 +0000

    04/26/21 09:42:01 - Update Graphs

--- run.sh
+++ run.sh
@@ -1,12 +1,10 @@
 #! /bin/bash
-exec >> "$BASE_PATH/log.txt"
-exec 2>&1
-
 NOW=$(date +"%D %T")
 echo $NOW
 
 echo 'Running graph Node.js app'
 cd $BASE_PATH
+git pull
 node index.js
 git commit -am "$NOW - Update Graphs"
 git push origin main
