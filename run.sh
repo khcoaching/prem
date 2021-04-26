27924926f3baf25c4ad796427b088dc36a72ebd5
khcoaching <khcoaching@github>
2021-04-26 17:41:20 +0900

    04/26/21 17:41:19

--- run.sh
+++ run.sh
@@ -2,6 +2,11 @@
 exec >> $KIMCHI_PATH/log.txt
 exec 2>&1
 
-date +"%D %T"
+NOW=$(date +"%D %T")
+echo $NOW
+
 echo 'Running graph Node.js app'
-cd $KIMCHI_PATH && node index.js
+cd $KIMCHI_PATH
+node index.js
+git commit -am "$NOW"
+git push origin main
